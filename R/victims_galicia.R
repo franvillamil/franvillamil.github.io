1b0640c50994ec4c99fa97ed584708cc3ea991c1
Francisco Villamil <francisco.villamil@icr.gess.ethz.ch>
2020-03-15 22:18:40 +0100

    Update victims_galicia.R

--- R/victims_galicia.R
+++ R/victims_galicia.R
@@ -1,43 +1,47 @@
-setwd("~/Documents/Academic/PhD/Projects/Spain/data")
 options(stringsAsFactors = FALSE)
 Sys.setlocale("LC_CTYPE", "C")
 library(muniSpain)
 
-## PREPARING
+## RETRIEVING DATA AND PREPARING
 
-# Read raw dataset
-# raw = read.csv("base-datos.csv",
-raw = read.csv("victims/raw_data/Galicia/datos_nomes_e_voces.csv",
+# Download
+url = "http://vitimas.nomesevoces.net/media/base-datos.csv"
+file = "victims_galicia_raw.csv"
+download.file(url, file)
+
+# Load
+data = read.csv("victims_galicia_raw.csv",
   col.names = c("nombre", "apellidos", "apodo", "tipo", "sexo",
   "edad", "profesion", "concello_nat", "comarca_nat", "prov_nat", "lugar",
   "concello_vecino", "comarca_vecino", "prov_vecino", "fecha", "info"))
-raw = adapt(raw[, c("nombre", "apellidos", "edad",
-  "concello_vecino", "tipo", "fecha")])
-raw$concello_vecino = tolower(raw$concello_vecino)
-raw$tipo = tolower(raw$tipo)
+data = adapt(data[, c("nombre", "apellidos", "edad",
+  "concello_vecino", "prov_vecino", "tipo", "fecha")])
+data$concello_vecino = tolower(data$concello_vecino)
+data$prov_vecino = tolower(data$prov_vecino)
+data$tipo = tolower(data$tipo)
 
 ## SUBSETTING
 
 # Only victims with known death date
-raw = subset(raw, fecha != "0000-00-00")
+data = subset(data, fecha != "0000-00-00")
 # NOTE: Data includes victims of non-fatal repression (did not die),
 # that's why the high numbers sometimes.
 
 # Transform to date class
-raw$fecha = as.Date(raw$fecha, "%Y-%m-%d")
+data$fecha = as.Date(data$fecha, "%Y-%m-%d")
 
 # Only 'paseos' and 'execucions'. Excluding deaths in prison and others.
-raw = subset(raw, tipo %in% c("paseo", "execucion"))
+data = subset(data, tipo %in% c("paseo", "execucion"))
 
 # Exclude deaths after 1942 (0.2%)
-raw = subset(raw, fecha <= "1942-12-31")
+data = subset(data, fecha <= "1942-12-31")
 
 ## ASSIGNING MUNICIPALITY CODES
 # (Following place where victims lived)
 
 # Exclude those without locality info
-raw$concello_vecino[raw$concello_vecino %in% c("", "desconecido")] = NA
-raw = subset(raw, !is.na(concello_vecino))
+data$concello_vecino[data$concello_vecino %in% c("", "desconecido")] = NA
+data = subset(data, !is.na(concello_vecino))
 
 # Converting names
 tmp = c("a estrada", "a pobra do caraminal", "o grove", "a coruna",
@@ -47,7 +51,24 @@ tmp = c("a estrada", "a pobra do caraminal", "o grove", "a coruna",
   "as somozas", "a mezquita", "a laracha", "o vicedo", "o pereiro de aguiar",
   "a arnoia", "a bana", "a rua", "a pobra de trives", "a gudina",
   "a veiga", "o carballino", "o irixo", "o bolo", "a pastoriza", "a lama")
-raw$concello_vecino[raw$concello_vecino %in% tmp] = gsub("(a|o|as) (.*)", "\\2, \\1",
-  raw$concello_vecino[raw$concello_vecino %in% tmp])
-raw$concello_vecino[raw$concello_vecino == "a illa de arousa"] = "illa de arousa (a)"
-raw$concello_vecino[raw$concello_vecino == "a merca"] = "merca , a"
+data$concello_vecino[data$concello_vecino %in% tmp] = gsub("(a|o|as) (.*)", "\\2, \\1",
+  data$concello_vecino[data$concello_vecino %in% tmp])
+data$concello_vecino[data$concello_vecino == "a illa de arousa"] = "illa de arousa (a)"
+data$concello_vecino[data$concello_vecino == "a merca"] = "merca, a"
+# Missing province names
+data$prov_vecino[data$concello_vecino == "corcubion" &
+  data$prov_vecino == ""] = "a coruna"
+data$prov_vecino[data$concello_vecino == "cee" &
+  data$prov_vecino == ""] = "a coruna"
+data$prov_vecino[data$concello_vecino == "mondariz-balneario" &
+  data$prov_vecino == ""] = "pontevedra"
+data$prov_vecino[data$concello_vecino == "fisterra" &
+  data$prov_vecino == ""] = "a coruna"
+
+# Assigning INE codes
+data$muni_code = name_to_code(data$concello_vecino, prov = data$prov_vecino)
+
+# Adapting to municipality changes between 1930 and 2011
+data$muni_code = changes_newcode(data$muni_code, 1930, 2011)
+
+write.csv(data, "victims_galicia.csv", row.names = FALSE)
